4f31d7523d5ce2b58f308c99b51e1142b0895c88
W-Nelissen <wne@sint-pieterscollege.be>
2024-01-30 16:05:16 +0100

    testing

--- chess_area_players.py
+++ chess_area_players.py
@@ -1,5 +1,12 @@
 from game_area import GameArea
+import pygame as pg
+pg.init()
+win = pg.display.set_mode((1200, 600))
 
+
+BROWN= ( 132, 94, 66)
+rect1 = pg.Rect((40, 80, 200, 75))  # (left,top,width,height)
+pg.draw.rect(win, BROWN , rect1, 4)
 """
    Dit is de game area waar alle spelerinformatie wordt getoond 
    player1: name, type, timer, ...
@@ -14,3 +21,4 @@ class GameAreaPlayers(GameArea):
         # Teken hier de Player informatie
         # Player 1: naam, tijd, ...
         # Player 2: naam, tijd, ...
+
