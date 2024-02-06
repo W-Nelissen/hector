8c9efc4a3dc322761feb0c500d2dc28000fc2644
Vivianne Michiels <vivianne.michiels@sint-pieterscollege.be>
2024-02-06 15:59:38 +0100

    klok vierkant

--- chess_area_players.py
+++ chess_area_players.py
@@ -1,5 +1,5 @@
 from game_area import GameArea
-
+import pygame as pg
 """
    Dit is de game area waar alle spelerinformatie wordt getoond 
    player1: name, type, timer, ...
@@ -14,3 +14,6 @@ class GameAreaPlayers(GameArea):
         # Teken hier de Player informatie
         # Player 1: naam, tijd, ...
         # Player 2: naam, tijd, ...
+        BROWN = (207, 146,133 )    
+        rect1 = pg.Rect((5, 200, 200, 75))  # (left,top,width,height)
+        pg.draw.rect(self.game.win, BROWN , rect1, 100)
