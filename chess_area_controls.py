5cb3669e130b1e21c97abb66bcdfe6d598c2cc1a
Chems-d <chems.dahhane@gmail.com>
2024-02-06 15:59:42 +0100

    mist een argument voor de rechthoek

--- chess_area_controls.py
+++ chess_area_controls.py
@@ -1,6 +1,7 @@
 from game_area import GameArea
 import pygame as pg
 pg.init()
+import colors
 
 
 
@@ -13,10 +14,10 @@ class GameAreaControls(GameArea):
 
     def draw(self):
         GameArea.draw(self)
-        
+        rect2 = pg.Rect((40,80,70,67))
+        pg.draw.rect(self.game.win, colors.RED, rect2 , 5)
         # Teken hier de controls
         # Save, Open, Quit, ...
 
 
 
-        
